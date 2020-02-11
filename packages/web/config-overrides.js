3ed122680042d481a51b719df58b67c8eb51c245
Paweł Galias <pawgalias@gmail.com>
2020-02-11 20:08:08 +0100

    chore: add prettier and stylelint to front end application

---
+++ packages/web/config-overrides.js
@@ -0,0 +1,11 @@
+const StylelintPlugin = require('stylelint-webpack-plugin');
+
+module.exports = {
+  webpack: function(config, env) {
+    if (env === 'development') {
+      config.plugins.push(new StylelintPlugin());
+    }
+
+    return config;
+  },
+};
